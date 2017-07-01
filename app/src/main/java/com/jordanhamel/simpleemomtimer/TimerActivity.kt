4e243ae529e01f9bb9153e1323f72017d15382ef
Jordan Hamel <jo.mravca@gmail.com>
2017-06-30 18:07:09 -0700

    Initial commit

---
+++ app/src/main/java/com/jordanhamel/simpleemomtimer/TimerActivity.kt
@@ -0,0 +1,90 @@
+package com.jordanhamel.simpleemomtimer
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+import kotlinx.android.synthetic.main.activity_timer.*
+import java.util.*
+
+class TimerActivity : AppCompatActivity(), View.OnClickListener {
+
+    val APP_NAME = "SimpleEMOMTimer"
+    val START_TIME = "start_time"
+    var DEFAULT_START_TIME : Long = -1
+    var startTime : Long = DEFAULT_START_TIME
+    var handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+        button.setOnClickListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startTime = getSavedStartTime()
+        updateUi()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveStartTime()
+        handler.removeCallbacks(null)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == button.id) {
+            if (startTime == DEFAULT_START_TIME) startTimer() else stopTimer()
+        }
+    }
+
+    private fun startTimer() {
+        startTime = Date().time
+        updateUi()
+    }
+
+    private fun stopTimer() {
+        startTime = DEFAULT_START_TIME
+        handler.removeCallbacks(null)
+        updateUi()
+    }
+
+    private fun updateUi() {
+        updateClock()
+        updateButton()
+    }
+
+    private fun updateButton() {
+        if (startTime == DEFAULT_START_TIME) {
+            button.setText(R.string.start)
+            button.setBackgroundResource(android.R.color.holo_green_dark)
+        } else {
+            button.setText(R.string.stop)
+            button.setBackgroundResource(android.R.color.holo_red_dark)
+        }
+    }
+
+    private fun updateClock() {
+        if (startTime == DEFAULT_START_TIME) {
+            clock.text = "0"
+        } else {
+            val now = Date().time
+            val secs: Int = ((now - startTime) / 1000 % 60).toInt()
+            clock.text = "$secs"
+            handler.postDelayed({ updateClock() }, 1000)
+        }
+    }
+
+    private fun saveStartTime() {
+        val sp = getSharedPreferences(APP_NAME, Context.MODE_PRIVATE)
+        sp.edit().putLong(START_TIME, startTime).apply()
+    }
+
+    private fun getSavedStartTime() : Long {
+        val sp = getSharedPreferences(APP_NAME, Context.MODE_PRIVATE)
+        return sp.getLong(START_TIME, DEFAULT_START_TIME)
+    }
+}
